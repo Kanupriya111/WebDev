b3f4505ed6319bee2ed50a86673274a16cece418
10_Kanupriya <kanupriya.sdm2006@gmail.com>
2025-09-24 20:01:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("you are welcomed!")
+alert("you are welcomed!");
+console.log("code is running....");
+console.log("code is also running....");
+var a = prompt("Enter your number");
+console.log("your number is " +a);
+var isTrue = confirm("are you sure you want to continue with this number");
+if(a%2==0 && isTrue){
+    console.log("This ia a even number");
+}else{
+    console.log("This is Odd Number");
+}
