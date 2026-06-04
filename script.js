6d15cf5375d2a8a6ed80c3abf1e0f45800b95220
10_Kanupriya <kanupriya.sdm2006@gmail.com>
2026-06-04 14:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -218,6 +218,7 @@ let arr2 = [1,2,3,4,5,6,7,8,9,10];
 const red = (a,b)=>{
     return a*b;
 }
+//push,pop,shift and unshift
 console.groupCollapsed(arr2.reduce(red));
 console.log(Array.from("Kanupriya"));
 
